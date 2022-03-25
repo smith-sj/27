549391fae2dc41c33e2962c9c13c8c8f23c13646
smith-sj <mr.sjsmith@protonmail.com>
2022-03-25 18:25:02 +1100

    refactor: moved game_key to validator class

--- game_validator.rb
+++ game_validator.rb
@@ -2,6 +2,21 @@ class GameValidator
 
     VALID_MOVES = ["1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c","4a", "4b", "4c", "5a", "5b", "5c", "6a", "6b", "6c","7a", "7b", "7c", "8a", "8b", "8c", "9a", "9b", "9c"]
 
+    GAME_KEY = [
+
+        [["1a","1b","1c"],
+        ["2a","2b","2c"],
+        ["3a","3b","3c"]],
+    
+        [["4a","4b","4c"],
+        ["5a","5b","5c"],
+        ["6a","6b","6c"]],
+    
+        [["7a","7b","7c"],
+        ["8a","8b","8c"],
+        ["9a","9b","9c"]],
+    ]
+
     # Checks if the move is included in valid moves
 
     def is_valid(move)
@@ -13,22 +28,22 @@ class GameValidator
 
     #converts move from "1a" to [0,0] etc.
 
-    def move_converter(move, game, key)
+    def move_converter(move)
         # make an array that we can edit, and return as a reference to where the move will go
         move_id = [0,0,0]
         # puts "starting move conversion"
 
-        key.each_with_index do |board,index|
+        GAME_KEY.each_with_index do |board,index|
             # puts "checking board #{key[index]} for #{move}"
-            if key[index].join.include? "#{move}"
+            if GAME_KEY[index].join.include? "#{move}"
                 # puts "found #{move}!"
                 # puts "move key_ID board is #{index}"
                 move_id[0] = index
-                key[index].each_with_index do |row,index2|
+                GAME_KEY[index].each_with_index do |row,index2|
                     if row.include? move
                         # puts "move key_ID row is #{index2}"
                         move_id[1] = index2
-                        key[index][index2].each_with_index do |column,index3|
+                        GAME_KEY[index][index2].each_with_index do |column,index3|
                             if column == move
                                 # puts "move key_ID column is #{index3}"
                                 move_id[2] = index3
@@ -44,8 +59,8 @@ class GameValidator
 
     # Checks if move has already been played
 
-    def is_unique(move, game, key)
-        move_key = move_converter(move, game, key)
+    def is_unique(move, game)
+        move_key = move_converter(move)
         # puts "current move key is #{move_key}"
         if game[move_key[0]][move_key[1]][move_key[2]] == " "
             return true
--- main.rb
+++ main.rb
@@ -13,22 +13,6 @@ NOUGHT = "o"
 
 # -----VARIABLES-----
 
-# A labeled game board for referencing
-GAME_KEY = [
-
-    [["1a","1b","1c"],
-    ["2a","2b","2c"],
-    ["3a","3b","3c"]],
-
-    [["4a","4b","4c"],
-    ["5a","5b","5c"],
-    ["6a","6b","6c"]],
-
-    [["7a","7b","7c"],
-    ["8a","8b","8c"],
-    ["9a","9b","9c"]],
-]
-
 # The current game that is being played
 board1 = GameBoard.new
 board1_squares = board1.squares
@@ -36,8 +20,8 @@ board1_squares = board1.squares
 
 # Add current move to game
 
-def add_move(player, move, game, key)
-    move_key = GameValidator.new.move_converter(move, game, key)
+def add_move(player, move, game)
+    move_key = GameValidator.new.move_converter(move)
     # puts "converted move is #{move_key}"
     game[move_key[0]][move_key[1]][move_key[2]] = player
 end
@@ -46,6 +30,7 @@ end
 def play_turn(player_x, player_o, player_name, token, board1_squares)
     x_tally = Adjudicator.new.tally_up(board1_squares)[:x_tally]
     o_tally = Adjudicator.new.tally_up(board1_squares)[:o_tally]
+
     # Print current game
     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
 
@@ -55,9 +40,9 @@ def play_turn(player_x, player_o, player_name, token, board1_squares)
         GameOutput.new.prompt_move(player_name)
         move = GameInput.new.get_move(token)
         # puts "move is currently #{move}"
-        if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+        if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares)
             # puts "move is valid and unique"
-            add_move(token, move, board1_squares, GAME_KEY)
+            add_move(token, move, board1_squares)
             # puts "added move"
             GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
             break
@@ -65,13 +50,13 @@ def play_turn(player_x, player_o, player_name, token, board1_squares)
             # puts "move is invalid"
             GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
             puts "\nInvalid Move! Try again.\n"
-        elsif !GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+        elsif !GameValidator.new.is_unique(move, board1_squares)
             # puts "stacking move on next board"
             move[0..0]= ((move[0]).to_i + 3).to_s
             # puts "new move is: #{move}"
-            if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+            if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares)
                 # puts "move is valid and unique"
-                add_move(token, move, board1_squares, GAME_KEY)
+                add_move(token, move, board1_squares)
                 # puts "added move"
                 GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
                 break
@@ -79,13 +64,13 @@ def play_turn(player_x, player_o, player_name, token, board1_squares)
                 # puts "move is invalid"
                 GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
                 puts "\nInvalid Move! Try again.\n"
-            elsif !GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+            elsif !GameValidator.new.is_unique(move, board1_squares)
                 # puts "stacking move on next board"
                 move[0..0]= ((move[0]).to_i + 3).to_s
                 # puts "new move is: #{move}"
-                if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+                if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares)
                     # puts "move is valid and unique"
-                    add_move(token, move, board1_squares, GAME_KEY)
+                    add_move(token, move, board1_squares)
                     # puts "added move"
                     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
                     break
@@ -93,7 +78,7 @@ def play_turn(player_x, player_o, player_name, token, board1_squares)
                     # puts "move is invalid"
                     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
                     puts "\nInvalid Move! Try again.\n"
-                elsif !GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+                elsif !GameValidator.new.is_unique(move, board1_squares)
                     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
                     puts "\nStack is full! Try again.\n"
                 end
@@ -106,55 +91,102 @@ def play_ai_turn(player_x, player_o, player_name, token, board1_squares)
 
     x_tally = Adjudicator.new.tally_up(board1_squares)[:x_tally]
     o_tally = Adjudicator.new.tally_up(board1_squares)[:o_tally]
+
     # Print current game
+
     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
     puts "\nWaiting for #{player_name}..."
+
     sleep rand(3..6)
+
      # Check move is valid and unique
+
      while true
-        # puts "prompting player"
+
         move = Ai.new.get_move
+
         # puts "move is currently #{move}"
-        if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+
+        if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares)
+
             # puts "move is valid and unique"
-            add_move(token, move, board1_squares, GAME_KEY)
+
+            add_move(token, move, board1_squares)
+
             # puts "added move"
+
             GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
             break
+
         elsif !GameValidator.new.is_valid(move)
+
             # puts "move is invalid"
+
             GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
             puts "\nInvalid Move! Try again.\n"
-        elsif !GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+
+        elsif !GameValidator.new.is_unique(move, board1_squares)
+
             # puts "stacking move on next board"
+
             move[0..0]= ((move[0]).to_i + 3).to_s
+
             # puts "new move is: #{move}"
-            if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+
+            if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares)
+
                 # puts "move is valid and unique"
-                add_move(token, move, board1_squares, GAME_KEY)
+
+                add_move(token, move, board1_squares)
+
                 # puts "added move"
                 GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
                 break
+
             elsif !GameValidator.new.is_valid(move)
+
                 # puts "move is invalid"
+
                 GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
                 puts "\nInvalid Move! Try again.\n"
-            elsif !GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+
+            elsif !GameValidator.new.is_unique(move, board1_squares)
+
                 # puts "stacking move on next board"
+
                 move[0..0]= ((move[0]).to_i + 3).to_s
+
                 # puts "new move is: #{move}"
-                if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+
+                if GameValidator.new.is_valid(move) && GameValidator.new.is_unique(move, board1_squares)
+                    
                     # puts "move is valid and unique"
-                    add_move(token, move, board1_squares, GAME_KEY)
+
+                    add_move(token, move, board1_squares)
+
                     # puts "added move"
+
                     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
                     break
+
                 elsif !GameValidator.new.is_valid(move)
+
                     # puts "move is invalid"
+
                     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
                     puts "\nInvalid Move! Try again.\n"
-                elsif !GameValidator.new.is_unique(move, board1_squares, GAME_KEY)
+
+                elsif !GameValidator.new.is_unique(move, board1_squares)
+
                     GameOutput.new.print_game(player_x, player_o, board1_squares, x_tally, o_tally)
+
                     puts "\nStack is full! Try again.\n"
                 end
             end
